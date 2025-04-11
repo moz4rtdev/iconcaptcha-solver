1e240e380d5e035dd25f4b42f8d6572a690aa12f
mallocdev <herdersonriker@gmail.com>
2025-04-11 13:26:19 -0300

    fix: update title in metadata and add head section for scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import './global.css';
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'IconCaptcha Solver API',
+  title: 'IconCaptcha Solver',
   description: 'API for solving IconCaptcha challenges',
 }
 
@@ -12,6 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1372819950640997"
+          crossOrigin="anonymous"></script>
+      </head>
       <body>{children}</body>
     </html>
   )
